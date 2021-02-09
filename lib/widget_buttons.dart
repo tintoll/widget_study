c839b366a269b31de5deb7c5401ea0c612085c30
tintoll <belselios@gmail.com>
2021-02-10 06:49:23 +0900

    add AlertDialog widget

--- lib/widget_buttons.dart
+++ lib/widget_buttons.dart
@@ -10,30 +10,49 @@ class WidgetButtons extends StatelessWidget {
       body: Column(
         children: [
           RaisedButton(
-              child : Text('RaisedButton'),
-              color : Colors.orange,
-              onPressed : () {
+              child: Text('RaisedButton'),
+              color: Colors.orange,
+              onPressed: () {
                 // 실행될 코드
-              }
-          ),
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('타이틀'),
+                      content: SingleChildScrollView(
+                        child: ListView(
+                          children: [Text('콘텐츠 영억입니다.'), Text('OK를 누르면 닫힙니다.')],
+                        ),
+                      ),
+                      actions: [
+                        FlatButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK')),
+                        FlatButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK')),
+                      ],
+                    );
+                  },
+                );
+              }),
           FlatButton(
-              child : Text('FlatButton'),
-              onPressed : () {
+              child: Text('FlatButton'),
+              onPressed: () {
                 // 실행될 코드
-              }
-          ),
+              }),
           IconButton(
-              icon : Icon(Icons.add),
-              color : Colors.red,
-              iconSize : 100.0,
-              onPressed : () {
+              icon: Icon(Icons.add),
+              color: Colors.red,
+              iconSize: 100.0,
+              onPressed: () {
                 // 실행될 코드
-              }
-          ),
-          FloatingActionButton(
-              child : Icon(Icons.add),
-              onPressed : () {}
-          )
+              }),
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {})
         ],
       ),
     );
